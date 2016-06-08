5e0c9f92456266fb556ec78921acebfd7cbee349
Toby Dylan Hocking <tdhock5@gmail.com>
2016-06-08 09:15:03 -0400

    new pkgs

--- packages.R
+++ packages.R
@@ -1,8 +1,11 @@
 works_with_R("3.2.2",
              data.table="1.9.6",
              lars="1.2",
-             "tdhock/animint@9fcc4c38bf8ad3b904443ce29cc4b241db6ce92b",
-             ElemStatLearn="2015.6.26",
-             ggplot2="1.0.1")
+             "hadley/scales@2c3edf45de56d617444dc38e47e0404173817886",
+             "tdhock/ggplot2@a8b06ddb680acdcdbd927773b1011c562134e4d2",
+             "tdhock/animint@6b1c9e588b03f632cd39cdec9bbcfa730db9e889",
+             ##ggplot2="1.0.1",
+             ##"tdhock/animint@9fcc4c38bf8ad3b904443ce29cc4b241db6ce92b",
+             ElemStatLearn="2015.6.26")
 
 library(class)
