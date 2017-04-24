b8fd8b8d937b5fc24a0e18a243e6c91b7e3371c8
Toby Dylan Hocking <tdhock5@gmail.com>
2017-04-24 12:06:24 -0400

    cleanup some TODOs, Newton

--- packages.R
+++ packages.R
@@ -1,8 +1,8 @@
 works_with_R("3.2.2",
              data.table="1.9.6",
              lars="1.2",
-             ggplot2="2.1.0",
-             "tdhock/animint@f11dd20b8938808f2cbe06beebb85ea9e8d67a0d",
+             "faizan-khan-iit/ggplot2@5fb99d0cece13239bbbc09c6b8a7da7f86ac58e2",
+             "tdhock/animint@8f066bd0c5aefff5d583c63dc9a121460ed4bb4e",
              ElemStatLearn="2015.6.26")
 
 library(class)
