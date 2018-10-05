0dcf4763e12503df2f6f1ac64cb0fc5155af53ca
Toby Dylan Hocking <tdhock5@gmail.com>
2018-10-05 11:40:25 -0700

    package update

--- packages.R
+++ packages.R
@@ -1,8 +1,17 @@
-works_with_R("3.2.2",
-             data.table="1.9.6",
-             lars="1.2",
-             "faizan-khan-iit/ggplot2@5fb99d0cece13239bbbc09c6b8a7da7f86ac58e2",
-             "tdhock/animint@8f066bd0c5aefff5d583c63dc9a121460ed4bb4e",
-             ElemStatLearn="2015.6.26")
+works_with_R(
+  "3.5.1",
+  data.table="1.11.8",
+  lars="1.2",
+  hexbin="1.27.2",
+  gtable="0.2.0",
+  scales="1.0.0",
+  reshape2="1.4.3",
+  plyr="1.8.4",
+  penaltyLearning="2018.9.4",
+  knitr="1.20",
+  "faizan-khan-iit/ggplot2@5fb99d0cece13239bbbc09c6b8a7da7f86ac58e2",
+  "tdhock/animint@9f3f84d8032f992784a4c0531ef117cdc2aa0d3f",
+  ##"tdhock/animint@8f066bd0c5aefff5d583c63dc9a121460ed4bb4e",
+  ElemStatLearn="2015.6.26")
 
 library(class)
