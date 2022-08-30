608477ea08132bfe2416ee0731a2426be29c49b6
Toby Dylan Hocking <tdhock5@gmail.com>
2022-08-29 22:39:24 -0700

    demo alpha_off and color_off

--- _plugins/knit.R
+++ _plugins/knit.R
@@ -1,3 +1,57 @@
+### Write down what package versions work with your R code, and
+### attempt to download and load those packages. The first argument is
+### the version of R that you used, e.g. "3.0.2" and then the rest of
+### the arguments are package versions. For
+### CRAN/Bioconductor/R-Forge/etc packages, write
+### e.g. RColorBrewer="1.0.5" and if RColorBrewer is not installed
+### then we use install.packages to get the most recent version, and
+### warn if the installed version is not the indicated version. For
+### GitHub packages, write "user/repo@commit"
+### e.g. "tdhock/animint@f877163cd181f390de3ef9a38bb8bdd0396d08a4" and
+### we use install_github to get it, if necessary.
+works_with_R <- function(Rvers,...){
+  local.lib <- file.path(getwd(), "library")
+  dir.create(local.lib, showWarnings=FALSE, recursive=TRUE)
+  .libPaths(c(local.lib, .libPaths()))
+  pkg_ok_have <- function(pkg,ok,have){
+    stopifnot(is.character(ok))
+    if(!as.character(have) %in% ok){
+      warning("works with ",pkg," version ",
+              paste(ok,collapse=" or "),
+              ", have ",have)
+    }
+  }
+  pkg_ok_have("R",Rvers,getRversion())
+  pkg.vers <- list(...)
+  for(pkg.i in seq_along(pkg.vers)){
+    vers <- pkg.vers[[pkg.i]]
+    pkg <- if(is.null(names(pkg.vers))){
+      ""
+    }else{
+      names(pkg.vers)[[pkg.i]]
+    }
+    if(pkg == ""){# Then it is from GitHub.
+      ## suppressWarnings is quieter than quiet.
+      if(!suppressWarnings(require(requireGitHub))){
+        ## If requireGitHub is not available, then install it using
+        ## devtools.
+        if(!suppressWarnings(require(devtools))){
+          install.packages("devtools")
+          require(devtools)
+        }
+        install_github("tdhock/requireGitHub")
+        require(requireGitHub)
+      }
+      requireGitHub(vers)
+    }else{# it is from a CRAN-like repos.
+      if(!suppressWarnings(require(pkg, character.only=TRUE))){
+        install.packages(pkg)
+      }
+      pkg_ok_have(pkg, vers, packageVersion(pkg))
+      library(pkg, character.only=TRUE)
+    }
+  }
+}
 works_with_R(
   "4.0.2",
   penaltyLearning="2020.5.13",
@@ -13,7 +67,7 @@ works_with_R(
   data.table="1.13.0",
   "rstudio/rmarkdown@c97053384dae1c0b254f73a354801bbb73176660",
   "hadley/bookdown@12ed348231f26d86e478b9462561cc6c8517992d",
-  "tdhock/animint2@0a2988ff02f73aab7ec6c3fb70d14439092bee27")
+  "tdhock/animint2@cd7f28e8dd3aba2d9f51e88c7404f414ecd8b2ab")
              
 library(methods)
 
